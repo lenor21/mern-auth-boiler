bcff9b5dc7cdf76bde36bf26044f7635c6bf0ebc
lenor21 <dejesusronel29@gmail.com>
2024-03-12 19:34:32 +0800

    connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+connectDB();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('server is running on port 5000');
 });
